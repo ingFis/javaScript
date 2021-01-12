c1d800ee652fc328f8b77ad64ebf2e5e89a32ee9
Ingrid <ingridprog98@gmail.com>
2021-01-12 16:15:48 -0300

    lala

--- Projeto_Compra_de_Pizzas/script.js
+++ Projeto_Compra_de_Pizzas/script.js
@@ -5,31 +5,32 @@ const cs = (e)=>{return document.querySelectorAll(e)}
 
 pizzaJson.map((pizza, index)=>{
     
-    // Clonar o modelo
+    // Parte 1 - Clonar o modelo - FAZER CÓPIA 1 
     let pizzariaItem = c('.models .pizza-item').cloneNode(true);
     
-    // Cria um atributo e pega o index da pizza
+    // Parte 3 - Cria um atributo e pega o index da pizza
     pizzariaItem.setAttribute('data-key', index);
 
-    // Preenche os itens
+    // Parte 1 - Preenche os itens 2
     pizzariaItem.querySelector('.pizza-item--img img').src = pizza.img
     pizzariaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${pizza.price.toFixed(2).replace('.', ',')}`
     pizzariaItem.querySelector('.pizza-item--name').innerHTML = pizza.name
     pizzariaItem.querySelector('.pizza-item--desc').innerHTML = pizza.description
 
-
-    // Quando clicar na imagem ou no ícone +
+    //Parte 2 - Quando clicar na imagem ou no ícone +
     pizzariaItem.querySelector('a').addEventListener('click', (e)=>{
 
         // Tira a ação natural de link(atualizar a página)
         e.preventDefault();
 
-        // A partir do a vai procurar o elemento mais próximo que tenha a classe pizza-item
+        // Parte 3 - A partir do a vai procurar o elemento mais próximo que tenha a classe pizza-item
         // e vai pegar o atributo data-key
         let key = e.target.closest('.pizza-item').getAttribute('data-key')
         modalQtd = 1;
 
-        // Preeche os campos da modal
+        // console.log(pizzaJson[key])
+
+        // Parte 3 - Preeche os campos da modal
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
@@ -38,19 +39,20 @@ pizzaJson.map((pizza, index)=>{
         // 1 - Resetar tamanho selecionado
         c('.pizzaInfo--size.selected').classList.remove('selected');
 
-        // 2 - Preeche os tamanhos da pizza
+        // Parte 4 - 2 - Preeche os tamanhos da pizza
         cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
 
             if(sizeIndex == 2){
                 size.classList.add('selected');
             }
 
+            // size é o próprio item, pois o cs retorna um array de elementos
             size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
         });
 
         c('.pizzaInfo--qt').innerHTML = modalQtd;
 
-        // Animação para abrir a modal de detalhes da pizza
+        //Parte 2 -  Animação para abrir a modal de detalhes da pizza - Acontece(0 para 1) em 0.5 segundo de acordo com o css
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
         setTimeout(()=>{
@@ -58,6 +60,6 @@ pizzaJson.map((pizza, index)=>{
         }, 200)
     })
     
-    // Adicionar os clones na area de pizza
+    // Parte 1 - Adicionar os clones na area de pizza - joga na tela 3
     c('.pizza-area').append(pizzariaItem);
 });
